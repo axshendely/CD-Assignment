c8f9472de27c9cf099a15e98de3be1b77d14abd1
axshendely <116259211+axshendely@users.noreply.github.com>
2023-10-01 21:12:38 +0200

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -2,8 +2,9 @@ echo "Somthing Changed!"
 cd /home/CD-Assignment
 echo "Printing working directory"
 pwd
-echo "restart application"
+echo "pull"
 git pull origin main
+echo "restart application"
 systemctl restart CD-Assignment
 systemctl status CD-Assignment    
 echo "You're up to date and application is restarted"
